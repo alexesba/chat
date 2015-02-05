93dd8942a6f29529f1724a27aba25c0c8c2c9094
alexesba <alexesba@gmail.com>
2015-02-05 17:58:42 -0600

    Added writter and reader

--- main.go
+++ main.go
@@ -6,22 +6,128 @@ import (
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
+	"time"
 	// "github.com/gin-gonic/gin"
 )
 
-var connections map[*websocket.Conn]bool
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
 
-func sendAll(msg []byte) {
-	for conn := range connections {
-		if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
-			delete(connections, conn)
-			return
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512
+)
+
+var connections map[*websocket.Conn]bool
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+type connection struct {
+	ws   *websocket.Conn
+	send chan []byte
+}
+
+type hub struct {
+	// Registered connections.
+	connections map[*connection]bool
+	// Inbound messages from the connections.
+	broadcast chan []byte
+	// Register requests from the connections.
+	register chan *connection
+	// Unregister requests from connections.
+	unregister chan *connection
+}
+
+var h = hub{
+	broadcast:   make(chan []byte),
+	register:    make(chan *connection),
+	unregister:  make(chan *connection),
+	connections: make(map[*connection]bool),
+}
+
+func (h *hub) run() {
+	for {
+		select {
+		case c := <-h.register:
+			h.connections[c] = true
+		case c := <-h.unregister:
+			if _, ok := h.connections[c]; ok {
+				delete(h.connections, c)
+				close(c.send)
+			}
+		case m := <-h.broadcast:
+			for c := range h.connections {
+				select {
+				case c.send <- m:
+				default:
+					close(c.send)
+					delete(h.connections, c)
+				}
+			}
+		}
+	}
+}
+
+func (c *connection) getMessage() {
+	defer func() {
+		h.unregister <- c
+		c.ws.Close()
+	}()
+	c.ws.SetReadLimit(maxMessageSize)
+	c.ws.SetReadDeadline(time.Now().Add(pongWait))
+	c.ws.SetPongHandler(func(string) error { c.ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		_, message, err := c.ws.ReadMessage()
+		if err != nil {
+			break
+		}
+		h.broadcast <- message
+	}
+}
+
+func (c *connection) write(mt int, payload []byte) error {
+	c.ws.SetWriteDeadline(time.Now().Add(writeWait))
+	return c.ws.WriteMessage(mt, payload)
+}
+
+func (c *connection) printMessage() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.ws.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			if !ok {
+				c.write(websocket.CloseMessage, []byte{})
+				return
+			}
+			if err := c.write(websocket.TextMessage, message); err != nil {
+				return
+			}
+		case <-ticker.C:
+			if err := c.write(websocket.PingMessage, []byte{}); err != nil {
+				return
+			}
 		}
 	}
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if _, ok := err.(websocket.HandshakeError); ok {
 		http.Error(w, "Not a websocket handshake", 400)
 		return
@@ -29,26 +135,18 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		return
 	}
-	defer conn.Close()
-	connections[conn] = true
-	for {
-		_, msg, err := conn.ReadMessage()
-		if err != nil {
-			delete(connections, conn)
-			return
-		}
-		log.Println(string(msg))
-		sendAll(msg)
-
-	}
+	c := &connection{send: make(chan []byte, 256), ws: conn}
+	h.register <- c
+	go c.printMessage()
+	c.getMessage()
 }
 
 func main() {
 	//command line flags
 	port := flag.Int("port", 8080, "port to serve on")
 	dir := flag.String("directory", "web/", "directory of web files")
+	go h.run()
 	flag.Parse()
-	connections = make(map[*websocket.Conn]bool)
 
 	// Handle all the requests by serving a file of the same name
 	fs := http.Dir(*dir)
